f0cf58647aaca600d911ce9f73b6dac5bb9658be
Shay-Jahen Merritte <Shay@Merritte.com>
2016-10-06 17:58:18 -0400

    added table view and set up basic task list

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Merritte on 10/6/16.
+//  Copyright © 2016 Merritte. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
